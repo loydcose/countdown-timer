f0696db21060dcd5d8e6efe3190c2cb66ac9add2
Loyd Cose <loydcose.01@gmail.com>
2022-04-15 11:04:01 +0800

    After adding hours

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
 let p = document.querySelector("p")
-const initialMinutes = 5
-let totalSeconds = 5 * 60
+
+const initialHours = 0
+const initialMinutes = 1
+
+let totalSeconds = initialHours * 60 * 60 + initialMinutes * 60
 
 let interval = setInterval(() => {
-  let minute = Math.floor(totalSeconds / 60)
+  let hour = Math.floor(totalSeconds / (60 * 60))
+  let minute = Math.floor((totalSeconds % (60 * 60)) / 60)
   let second = totalSeconds % 60
 
   second = second < 10 ? "0" + second : second
-  minute = minute < 10 ? '0' + minute : minute
+  minute = minute < 10 ? "0" + minute : minute
+  hour = hour < 10 ? "0" + hour : hour
 
-  if (minute == 0 && second == 0) {
+  if (minute == 0 && second == 0 && hour == 0) {
     clearInterval(interval)
   }
 
-  p.innerText = `${minute}:${second}`
+  p.innerText = `${hour}:${minute}:${second}`
   totalSeconds--
-
-
 }, 100)
-
-/*
-const initialHour  = 2 // 2h
-const initialMinute  = 30// 30mn
-
-totalSeconds = (initialHour * 60 * 60) + (initialMinute * 60)
-
-interval
-  hour = Math.floor(totalSeconds / (60 * 60))
-  minute = Math.floor(totalSeconds % hour)
-  second = totalSeconds % minute
-
-  then print the values
-
-
-*/
